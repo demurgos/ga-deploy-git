39ec0a5f564fe938d4ece91a31719945c96b2256
Charles Samborski <demurgos@demurgos.net>
2019-09-20 12:48:55 +0200

    Release v1.0.0
    - **[Feature]** First release.

--- README.md
+++ README.md
@@ -1,4 +1,107 @@
-# Github Action to deploy a project using Git
+# GitHub Action to deploy a project using Git
 
-Only supports deploying to Github repositories for the moment.
-Can be used to deploy to Github Pages.
+This action allows you to deploy a directory as the content of a Git branch.
+This only supports deployment across repositories hosted on GitHub.
+
+You can use it for example to deploy to GitHub Pages.
+
+It checks out a remote branch, updates its content, creates a commit and pushes
+it.
+
+It is implemented as a Node action using only the official actions toolkit and
+Node's core modules: it is fast and runs on every platform.
+
+## Inputs
+
+### `accessToken`
+
+**Required**
+
+Personal access token with read/write access to the repo (`public_repo` or
+`repo scope` depending on the visibility of the repo).
+
+See <https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line>
+
+**Store it in a _secret_.** You can configure secrets in your repository settings.
+
+### `srcDir`
+
+Directory in the source branch containing the files to deploy.
+
+Default: `"."` (repository root)
+
+### `destRepo`
+
+Destination repo slug in the form `"userOrOrganization/repoName"`.
+
+Default: repo that triggered the workflow.
+
+### `destBranch`
+
+**Required**
+
+Destination branch: the files will be deployed at its root.
+
+### `commitUser`
+
+Git `user.name` value for the deployment commit.
+
+Default: login of the user that triggered the workflow.
+
+### `commitEmail`
+
+Git `user.email` value for the deployment commit.
+
+Default: `<userLogin>@users.noreply.github.com`.
+
+## Example
+
+This library was developed to support deploying a website build to GitHub Pages.
+As part of your workflow, you can build the website to a directory and then
+deploy this directory as the content of the `gh-pages` branch (or `master`
+branch of `user/user.github.io`).
+
+Here is an example workflow to deploy from a repository containing website
+sources and deploying to `user/user.github.io`.
+
+```yml
+name: "Build and Deploy"
+# Only deploy from `master`
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+      # Checkout the branch that triggered the workflow.
+      - name: Checkout
+        uses: actions/checkout@master
+
+      # Run your build step
+      - name: Build
+        run: ./build.sh
+
+      # Deploy using `demurgos/ga-deploy-git`
+      - name: Deploy
+        uses: demurgos/ga-deploy-git@v1
+        with:
+          # Personnal access token with `public_repo` or `repo` permission (required)
+          # It is defined in the `ACCESS_TOKEN` secret.
+          accessToken: ${{ secrets.ACCESS_TOKEN }}
+          # Directory containing the build artifact to deploy (default: repo root)
+          srcDir: build
+          # Destination repo (default: current repo)
+          destRepo: user/user.github.io
+          # Destination branch (required)
+          destBranch: master
+```
+
+# License
+
+Dual-licensed at your choice:
+
+- [MIT](https://spdx.org/licenses/MIT)
+- [Apache-2.0](https://spdx.org/licenses/Apache-2.0)
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import core from "@actions/core";
-import exec from "@actions/exec";
-import github from "@actions/github";
-import io from "@actions/io";
-import sysPath from "path";
-import fs from "fs";
-import os from "os";
-import crypto from "crypto";
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as github from "@actions/github";
+import * as io from "@actions/io";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as os from "os";
+import * as sysPath from "path";
 
 async function main() {
   const userInputs: Inputs = getInputs();
@@ -15,60 +15,153 @@ async function main() {
 
 interface Inputs {
   accessToken: string;
-  srcBranch: string | undefined;
   srcDir: string | undefined;
+  destRepo: string | undefined;
   destBranch: string;
+  commitUser: string | undefined;
+  commitEmail: string | undefined;
 }
 
 function getInputs(): Inputs {
   const accessToken: string = core.getInput("accessToken", {required: true});
-  const srcBranch: string | undefined = core.getInput("srcBranch", {required: false});
-  const srcDir: string | undefined = core.getInput("srcBranch", {required: false});
+  const srcDir: string | undefined = core.getInput("srcDir", {required: false});
+  const destRepo: string | undefined = core.getInput("destRepo", {required: false});
   const destBranch: string = core.getInput("destBranch", {required: true});
-  return {accessToken, srcBranch, srcDir, destBranch};
+  const commitUser: string | undefined = core.getInput("commitUser", {required: false});
+  const commitEmail: string | undefined = core.getInput("commitEmail", {required: false});
+  return {accessToken, srcDir, destRepo, destBranch, commitUser, commitEmail};
 }
 
 interface ResolvedInputs {
   accessToken: string;
-  srcBranch: string;
   srcDir: string;
+  destRepo: string;
   destBranch: string;
+  commitUser: string;
+  commitEmail: string;
 }
 
 function resolveInputs(inputs: Inputs): ResolvedInputs {
-  let srcBranch: string;
-  if (inputs.srcBranch !== undefined) {
-    srcBranch = inputs.srcBranch;
-  } else {
-    // https://help.github.com/en/articles/events-that-trigger-workflows
-    // https://developer.github.com/v3/activity/events/types
-    if (github.context.eventName === "push") {
-      srcBranch = github.context.ref;
-    } else {
-      throw new Error(`Unable to resolve default \`srcBranch\` input for non \`push\` event: ${github.context.eventName}`);
-    }
-  }
-  const srcDir: string = inputs.srcDir !== undefined ? inputs.srcDir : ".";
-  return {...inputs, srcBranch, srcDir};
+  let destRepo: string = isDefined(inputs.destRepo)
+    ? inputs.destRepo
+    : `${github.context.repo.owner}/${github.context.repo.repo}`;
+  const srcDir: string = isDefined(inputs.srcDir) ? inputs.srcDir : ".";
+  const commitUser: string = isDefined(inputs.commitUser)
+    ? inputs.commitUser
+    : github.context.actor;
+  const commitEmail: string = isDefined(inputs.commitEmail)
+    ? inputs.commitEmail
+    : `${github.context.actor}@users.noreply.github.com`;
+  return {...inputs, srcDir, destRepo, commitUser, commitEmail};
+}
+
+function isDefined(optStr: string | undefined): optStr is string {
+  return optStr !== undefined && optStr.length > 0;
 }
 
 async function deploy(inputs: ResolvedInputs): Promise<void> {
-  const destRepoSlug: string = `${github.context.repo.owner}/${github.context.repo.repo}`; // TODO: Allow to configure it
-  const destRepoUri: string = `https://${inputs.accessToken}@github.com/${destRepoSlug}.git`;
+  core.debug(JSON.stringify(inputs));
 
-  return withTmpDir<void>(async (tmpDir: string): Promise<void> => {
-    // Clone dest repository
-    if (await exec.exec("git", ["clone", destRepoUri, tmpDir]) !== 0) {
-      throw new Error(`Failed to clone destination repo: ${destRepoSlug}`);
+  const destRepoUri: string = `https://${inputs.accessToken}@github.com/${inputs.destRepo}.git`;
+
+  return withTmpDir<void>(async (cwd: string): Promise<void> => {
+    core.info("Initializing destination repository");
+
+    await exec.exec("git", ["init"], {cwd});
+    await exec.exec("git", ["config", "--local", "user.name", inputs.commitUser], {cwd});
+    await exec.exec("git", ["config", "--local", "user.email", inputs.commitEmail], {cwd});
+    await exec.exec("git", ["remote", "add", "dest", destRepoUri], {cwd});
+    await exec.exec("git", ["fetch", "dest"], {cwd});
+
+    let destBranchExists: boolean;
+    {
+      const exitCode: number = await exec.exec(
+        "git",
+        ["ls-remote", "--heads", "--quiet", "--exit-code", "dest", inputs.destBranch],
+        {cwd, ignoreReturnCode: true},
+      );
+      switch (exitCode) {
+        case 0: {
+          destBranchExists = true;
+          break;
+        }
+        case 2: {
+          destBranchExists = false;
+          break;
+        }
+        default:
+          throw new Error("Failed to check existence of `destBranch` in destination repository");
+      }
     }
-    console.log("Done");
+    let isNewBranch: boolean;
+    if (destBranchExists) {
+      core.info(`Checking destination branch: ${inputs.destBranch}`);
+      await exec.exec("git", ["checkout", "-t", `dest/${inputs.destBranch}`], {cwd});
+      isNewBranch = false;
+    } else {
+      core.info(`Creating destination branch (not found in destination repo): ${inputs.destBranch}`);
+      await exec.exec("git", ["checkout", "--orphan", inputs.destBranch], {cwd});
+      isNewBranch = true;
+    }
+
+    core.info(`Setting destination content from source directory: ${inputs.srcDir}`);
+    await rmAllExceptDotGit(cwd);
+    const isNonEmpty: boolean = await copyAllExceptDotGit(inputs.srcDir, cwd);
+    await exec.exec("git", ["add", "."], {cwd});
+
+    let hasChanges: boolean;
+    if (isNewBranch) {
+      hasChanges = isNonEmpty;
+    } else {
+      const exitCode: number = await exec.exec(
+        "git",
+        ["diff-index", "--quiet", "HEAD", "--"],
+        {cwd, ignoreReturnCode: true, silent: true},
+      );
+      hasChanges = exitCode != 0;
+    }
+
+    if (hasChanges) {
+      core.info("Creating deployment commit");
+      const sha: string = github.context.sha;
+      const commitTitle: string = await getCommitTitle(sha);
+      const shortSha: string = sha.substr(0, 7);
+      const msgLines: ReadonlyArray<string> = [
+        `Deploy(${shortSha}): ${commitTitle}`,
+        "",
+        `Repo: ${github.context.repo.owner}/${github.context.repo.repo}`,
+        `Workflow: ${github.context.workflow}`,
+        `SHA: ${sha}`,
+        `Ref: ${github.context.ref}`,
+        `Actor: ${github.context.actor}`,
+        "",
+      ];
+
+      const msg: string = msgLines.join("\n");
+      await exec.exec("git", ["commit", "-m", msg], {cwd});
+      core.info("Deploying");
+      await exec.exec("git", ["push", "dest", inputs.destBranch], {cwd});
+    } else {
+      core.info("Skipping deployment: no changes detected");
+    }
   });
+}
+
+async function getCommitTitle(sha: string): Promise<string> {
+  const chunks: Uint8Array[] = [];
+  await exec.exec(
+    "git",
+    ["log", "--format=%B", "-n", "1", sha],
+    {silent: true, listeners: {stdout: data => chunks.push(data)}},
+  );
+  const message: string = Buffer.concat(chunks).toString("utf8");
+  return message.split("\n")[0].trim();
 }
 
 async function withTmpDir<T>(fn: (dirPath: string) => Promise<T>): Promise<T> {
   const tmpDir: string = createTmpDirSync();
   try {
-    return fn(tmpDir);
+    return await fn(tmpDir);
   } finally {
     await io.rmRF(tmpDir);
   }
@@ -80,7 +173,7 @@ function createTmpDirSync(): string {
   let tryCount: number = 0;
   while (tryCount < MAX_TRIES) {
     tryCount++;
-    const name: string = crypto.randomBytes(8).toString("hex");
+    const name: string = `.tmp-${crypto.randomBytes(8).toString("hex")}`;
     const tmpDir: string = sysPath.join(tmpRoot, name);
     try {
       fs.mkdirSync(tmpDir);
@@ -94,6 +187,33 @@ function createTmpDirSync(): string {
     }
   }
   throw new Error("Failed to create temporary directory");
+}
+
+async function rmAllExceptDotGit(dir: string): Promise<void> {
+  const fileNames: string[] = fs.readdirSync(dir);
+  const rmPromises: Promise<void>[] = [];
+  for (const fileName of fileNames) {
+    if (fileName === ".git") {
+      continue;
+    }
+    rmPromises.push(io.rmRF(sysPath.join(dir, fileName)));
+  }
+  await Promise.all(rmPromises);
+}
+
+async function copyAllExceptDotGit(srcDir: string, destDir: string): Promise<boolean> {
+  let didCopySomething: boolean = false;
+  const fileNames: string[] = fs.readdirSync(srcDir);
+  const cpPromises: Promise<void>[] = [];
+  for (const fileName of fileNames) {
+    if (fileName === ".git") {
+      continue;
+    }
+    didCopySomething = true;
+    cpPromises.push(io.cp(sysPath.join(srcDir, fileName), sysPath.join(destDir, fileName)));
+  }
+  await Promise.all(cpPromises);
+  return didCopySomething;
 }
 
 (async () => {
